2c5380ce7e81307b918ebac3afda324f30a04ae5
YI <uuuuuu@protonmail.com>
2021-10-20 17:24:05 +0800

    add default.nix for generated flake pkgs

--- nix/common.nix
+++ nix/common.nix
@@ -74,6 +74,13 @@ let
     cat <<"EOF" >$out/flake.nix
     {inputs=${inputsCode};outputs=${outputsCode};}
     EOF
+    cat <<"EOF" >$out/default.nix
+    (import (fetchTarball {
+      url =
+        "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
+      sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2";
+    }) { src = ./.; }).defaultNix.legacyPackages.${config.nixpkgs.system}
+    EOF
     cat <<"EOF" >$out/flake.lock
     ${lockJSON}
     EOF
