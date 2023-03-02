c85a632a7848ba42b2a6fcb00dae334a180ce696
YI <uuuuuu@protonmail.com>
2023-03-02 10:22:03 +0800

    update nix-on-droid to 22.11

--- flake.nix
+++ flake.nix
@@ -121,7 +121,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, flake-utils, gomod2nix, rust-overlay, crate2nix, nix-alien, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, flake-utils, gomod2nix, rust-overlay, crate2nix, nix-alien, nix-on-droid, ... }@inputs:
     let
       lib = nixpkgs.lib;
 
@@ -213,93 +213,98 @@
       }
       {
         nixOnDroidConfigurations = {
-          nod = inputs.nix-on-droid.lib.nixOnDroidConfiguration {
-            config = { pkgs, lib, config, ... }:
-              let
-                sshdTmpDirectory = "${config.user.home}/.sshd-tmp";
-                sshdDirectory = "${config.user.home}/.sshd";
-                dotfilesDirectory = "${config.user.home}/.local/share/chezmoi";
-                dotfilesRepo = "https://github.com/contrun/dotfiles";
-                githubUser = "contrun";
-                port = 8822;
-              in
-              {
-                build.activation.sshd = ''
-                  $DRY_RUN_CMD mkdir $VERBOSE_ARG --parents "${config.user.home}/.ssh"
-                  if [[ ! -f "${config.user.home}/.ssh/authorized_keys" ]]; then
-                    # ssh-import-id requires ssh-keygen
-                    if ! PATH="${lib.makeBinPath [ pkgs.openssh ]}:$PATH" $DRY_RUN_CMD ${pkgs.ssh-import-id}/bin/ssh-import-id -o "${config.user.home}/.ssh/authorized_keys" "gh:${githubUser}"; then
-                      $VERBOSE_ECHO "Importing ssh key from ${githubUser} failed"
+          default = inputs.nix-on-droid.lib.nixOnDroidConfiguration {
+            modules = [
+              ({ pkgs, lib, config, ... }:
+                let
+                  sshdTmpDirectory = "${config.user.home}/.sshd-tmp";
+                  sshdDirectory = "${config.user.home}/.sshd";
+                  dotfilesDirectory = "${config.user.home}/.local/share/chezmoi";
+                  dotfilesRepo = "https://github.com/contrun/dotfiles";
+                  githubUser = "contrun";
+                  port = 8822;
+                in
+                {
+                  build.activation.sshd = ''
+                    $DRY_RUN_CMD mkdir $VERBOSE_ARG --parents "${config.user.home}/.ssh"
+                    if [[ ! -f "${config.user.home}/.ssh/authorized_keys" ]]; then
+                      # ssh-import-id requires ssh-keygen
+                      if ! PATH="${lib.makeBinPath [ pkgs.openssh ]}:$PATH" $DRY_RUN_CMD ${pkgs.ssh-import-id}/bin/ssh-import-id -o "${config.user.home}/.ssh/authorized_keys" "gh:${githubUser}"; then
+                        $VERBOSE_ECHO "Importing ssh key from ${githubUser} failed"
+                      fi
                     fi
-                  fi
 
-                  if [[ ! -d "${sshdDirectory}" ]]; then
-                    $DRY_RUN_CMD rm $VERBOSE_ARG --recursive --force "${sshdTmpDirectory}"
-                    $DRY_RUN_CMD mkdir $VERBOSE_ARG --parents "${sshdTmpDirectory}"
+                    if [[ ! -d "${sshdDirectory}" ]]; then
+                      $DRY_RUN_CMD rm $VERBOSE_ARG --recursive --force "${sshdTmpDirectory}"
+                      $DRY_RUN_CMD mkdir $VERBOSE_ARG --parents "${sshdTmpDirectory}"
 
-                    $VERBOSE_ECHO "Generating host keys..."
-                    $DRY_RUN_CMD ${pkgs.openssh}/bin/ssh-keygen -t rsa -b 4096 -f "${sshdTmpDirectory}/ssh_host_rsa_key" -N ""
+                      $VERBOSE_ECHO "Generating host keys..."
+                      $DRY_RUN_CMD ${pkgs.openssh}/bin/ssh-keygen -t rsa -b 4096 -f "${sshdTmpDirectory}/ssh_host_rsa_key" -N ""
 
-                    $VERBOSE_ECHO "Writing sshd_config..."
-                    $DRY_RUN_CMD ${pkgs.python3}/bin/python -c 'with open("${sshdTmpDirectory}/sshd_config", "w") as f: f.write("HostKey ${sshdDirectory}/ssh_host_rsa_key\nPort ${toString port}\n")'
+                      $VERBOSE_ECHO "Writing sshd_config..."
+                      $DRY_RUN_CMD ${pkgs.python3}/bin/python -c 'with open("${sshdTmpDirectory}/sshd_config", "w") as f: f.write("HostKey ${sshdDirectory}/ssh_host_rsa_key\nPort ${toString port}\n")'
 
-                    $DRY_RUN_CMD mv $VERBOSE_ARG "${sshdTmpDirectory}" "${sshdDirectory}"
-                  fi
-                '';
-
-                build.activation.dotfiles = ''
-                  if [[ ! -d "${dotfilesDirectory}" ]]; then
-                    $DRY_RUN_CMD mkdir $VERBOSE_ARG --parents "${dotfilesDirectory}"
-                    if ! $DRY_RUN_CMD ${pkgs.git}/bin/git clone "${dotfilesRepo}" "${dotfilesDirectory}"; then
-                      $VERBOSE_ECHO "Cloning repo ${dotfilesRepo} into ${dotfilesDirectory} failed"
+                      $DRY_RUN_CMD mv $VERBOSE_ARG "${sshdTmpDirectory}" "${sshdDirectory}"
                     fi
-                    if ! PATH="${lib.makeBinPath [ pkgs.coreutils pkgs.diffutils pkgs.gnupg pkgs.gnugrep pkgs.gnused pkgs.curl pkgs.chezmoi pkgs.git ]}:$PATH" $DRY_RUN_CMD ${pkgs.gnumake}/bin/make -C "${dotfilesDirectory}" home-install; then
-                      $VERBOSE_ECHO "Installing dotfiles failed"
-                    fi
-                  fi
-                '';
+                  '';
 
-                environment.packages = with pkgs; [
-                  git
-                  man
-                  openssh
-                  gnupg
-                  mosh
-                  coreutils
-                  rsync
-                  diffutils
-                  gnugrep
-                  gnused
-                  gawk
-                  curl
-                  neovim
-                  chezmoi
-                  gnumake
-                  (writeShellApplication {
-                    name = "sshd-start";
-                    text = ''
-                      ip -brief addr show scope global up
-                      echo "Starting sshd on port ${toString port}"
-                      # sshd re-exec requires execution with an absolute path
-                      exec ${pkgs.openssh}/bin/sshd -f "${sshdDirectory}/sshd_config" -D "$@"
-                    '';
-                    runtimeInputs = with pkgs; [ iproute2 openssh ];
-                  })
-                  self.packages."aarch64-linux".mosha
-                  self.packages."aarch64-linux".ssha
-                ];
-                system.stateVersion = "22.05";
-              };
-            system = "aarch64-linux";
-            extraModules = [
-              # import source out-of-tree modules like:
-              # flake.nixOnDroidModules.module
+                  build.activation.dotfiles = ''
+                    if [[ ! -d "${dotfilesDirectory}" ]]; then
+                      $DRY_RUN_CMD mkdir $VERBOSE_ARG --parents "${dotfilesDirectory}"
+                      if ! $DRY_RUN_CMD ${pkgs.git}/bin/git clone "${dotfilesRepo}" "${dotfilesDirectory}"; then
+                        $VERBOSE_ECHO "Cloning repo ${dotfilesRepo} into ${dotfilesDirectory} failed"
+                      fi
+                      if ! PATH="${lib.makeBinPath [ pkgs.coreutils pkgs.diffutils pkgs.gnupg pkgs.gnugrep pkgs.gnused pkgs.curl pkgs.chezmoi pkgs.git ]}:$PATH" $DRY_RUN_CMD ${pkgs.gnumake}/bin/make -C "${dotfilesDirectory}" home-install; then
+                        $VERBOSE_ECHO "Installing dotfiles failed"
+                      fi
+                    fi
+                  '';
+
+                  environment.packages = with pkgs; [
+                    git
+                    man
+                    openssh
+                    gnupg
+                    mosh
+                    coreutils
+                    rsync
+                    diffutils
+                    gnugrep
+                    gnused
+                    gawk
+                    curl
+                    neovim
+                    chezmoi
+                    gnumake
+                    (writeShellApplication {
+                      name = "sshd-start";
+                      text = ''
+                        ip -brief addr show scope global up
+                        echo "Starting sshd on port ${toString port}"
+                        # sshd re-exec requires execution with an absolute path
+                        exec ${pkgs.openssh}/bin/sshd -f "${sshdDirectory}/sshd_config" -D "$@"
+                      '';
+                      runtimeInputs = with pkgs; [ iproute2 openssh ];
+                    })
+                  ];
+                  system.stateVersion = "22.05";
+                })
             ];
-            extraSpecialArgs = {
-              # arguments to be available in every nix-on-droid module
+            # set nixpkgs instance, it is recommended to apply `nix-on-droid.overlays.default`
+            pkgs = import nixpkgs {
+              system = "aarch64-linux";
+
+              overlays = [
+                nix-on-droid.overlays.default
+                (final: prev: {
+                  mosh = self.packages."aarch64-linux".mosh;
+                  ssh = self.packages."aarch64-linux".ssh;
+                })
+              ];
             };
-            # your own pkgs instance (see nix-on-droid.overlay for useful additions)
-            # pkgs = ...;
+
+            # set path to home-manager flake
+            home-manager-path = home-manager.outPath;
           };
         };
       }
