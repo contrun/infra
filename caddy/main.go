2cfaa112c6340844be90a5cc865c288a8db7294a
B YI <uuuuuu@protonmail.com>
2023-08-12 01:32:40 +0000

    caddy: remove some modules to build successfully

--- caddy/main.go
+++ caddy/main.go
@@ -4,9 +4,7 @@ import (
 	caddycmd "github.com/caddyserver/caddy/v2/cmd"
 
 	// plug in Caddy modules here
-	_ "github.com/caddyserver/caddy/v2/modules/standard"
 	_ "github.com/greenpau/caddy-security"
-	_ "github.com/lucaslorentz/caddy-docker-proxy/plugin"
 	_ "github.com/mholt/caddy-l4"
 )
 
