3558ddf70736fa3885bc60cc6724617eb9f9e19f
YI <uuuuuu@protonmail.com>
2024-12-13 21:40:33 +0800

    neovim: change diffview config

--- home/dot_config/nvim/lua/plugins.lua
+++ home/dot_config/nvim/lua/plugins.lua
@@ -565,7 +565,46 @@ require("lazy").setup({
 
     { 'fidian/hexmode' },
 
-    { 'sindrets/diffview.nvim',                   dependencies = 'nvim-lua/plenary.nvim' },
+    {
+      'sindrets/diffview.nvim',
+      dependencies = 'nvim-lua/plenary.nvim',
+      config = function()
+        local actions = require("diffview.actions")
+
+        require("diffview").setup({
+          view = {
+            -- Configure the layout and behavior of different types of views.
+            -- Available layouts:
+            --  'diff1_plain'
+            --    |'diff2_horizontal'
+            --    |'diff2_vertical'
+            --    |'diff3_horizontal'
+            --    |'diff3_vertical'
+            --    |'diff3_mixed'
+            --    |'diff4_mixed'
+            -- For more info, see |diffview-config-view.x.layout|.
+            default = {
+              -- Config for changed files, and staged files in diff views.
+              layout = "diff2_horizontal",
+              disable_diagnostics = false, -- Temporarily disable diagnostics for diff buffers while in the view.
+              winbar_info = false,         -- See |diffview-config-view.x.winbar_info|
+            },
+            merge_tool = {
+              -- Config for conflicted files in diff views during a merge or rebase.
+              layout = "diff3_mixed",
+              disable_diagnostics = true, -- Temporarily disable diagnostics for diff buffers while in the view.
+              winbar_info = true,         -- See |diffview-config-view.x.winbar_info|
+            },
+            file_history = {
+              -- Config for changed files in file history views.
+              layout = "diff2_horizontal",
+              disable_diagnostics = false, -- Temporarily disable diagnostics for diff buffers while in the view.
+              winbar_info = false,         -- See |diffview-config-view.x.winbar_info|
+            },
+          },
+        })
+      end
+    },
 
     {
       'NeogitOrg/neogit',
