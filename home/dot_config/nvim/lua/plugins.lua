89541f87f1f0d6f5252b7dea0753f4314b2cc053
YI <uuuuuu@protonmail.com>
2024-12-17 14:58:52 +0800

    neovim: add plugin for term colors

--- home/dot_config/nvim/lua/plugins.lua
+++ home/dot_config/nvim/lua/plugins.lua
@@ -705,6 +705,21 @@ require("lazy").setup({
 
     { 'github/copilot.vim' },
 
+    {
+      "m00qek/baleia.nvim",
+      version = "*",
+      config = function()
+        vim.g.baleia = require("baleia").setup({})
+
+        -- Command to colorize the current buffer
+        vim.api.nvim_create_user_command("BaleiaColorize", function()
+          vim.g.baleia.once(vim.api.nvim_get_current_buf())
+        end, { bang = true })
+
+        -- Command to show logs
+        vim.api.nvim_create_user_command("BaleiaLogs", vim.g.baleia.logger.show, { bang = true })
+      end,
+    }
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
