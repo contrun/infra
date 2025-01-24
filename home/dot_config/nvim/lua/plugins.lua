0a64190ba22eb915a4a78b2ee67987e4d5e66e91
YI <uuuuuu@protonmail.com>
2025-01-24 11:47:41 +0800

    Neovim: add more shortcuts for gitlinker

--- home/dot_config/nvim/lua/plugins.lua
+++ home/dot_config/nvim/lua/plugins.lua
@@ -637,6 +637,12 @@ require("lazy").setup({
       dependencies = 'nvim-lua/plenary.nvim',
       config = function()
         require "gitlinker".setup({ mappings = "<leader>gy" })
+        vim.api.nvim_set_keymap('n', '<leader>go',
+          '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+          { silent = true })
+        vim.api.nvim_set_keymap('v', '<leader>go',
+          '<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+          { silent = true })
       end
     },
 
