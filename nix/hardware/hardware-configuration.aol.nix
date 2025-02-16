24f42fe0cc4c66fa06ba844723d8681d453817b4
YI <uuuuuu@protonmail.com>
2025-02-17 03:46:05 +0800

    nixos: fix sway screen glitches by disabling i915 psr

--- nix/hardware/hardware-configuration.aol.nix
+++ nix/hardware/hardware-configuration.aol.nix
@@ -64,6 +64,9 @@
 
   services.xserver.videoDrivers = [ "nvidia" ];
 
+  # https://bbs.archlinux.org/viewtopic.php?pid=2093176#p2093176
+  boot.kernelParams = [ "i915.enable_psr=0" ];
+
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 
   hardware = {
