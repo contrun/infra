867f2146b6d7467e7dae341c008582df5933a66e
YI <uuuuuu@protonmail.com>
2025-08-17 21:51:15 +0800

    home/wezterm: fix capturing markdown link in hyperlink rules

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -73,7 +73,47 @@ if is_windows then
   dev_domain = sshmux_domain
 end
 
-local config = {}
+local config = wezterm.config_builder()
+
+-- [default hyperlink_rules is wrong for Markdown link format · Issue #3803 · wezterm/wezterm](https://github.com/wezterm/wezterm/issues/3803)
+config.hyperlink_rules = {
+  -- Matches: a URL in parens: (URL)
+  -- Markdown: [text](URL title)
+  {
+    regex = '\\((\\w+://[^\\s\\)]+)(?:\\s+[^\\)]*)?\\)',
+    format = '$1',
+    highlight = 1,
+  },
+  -- Matches: a URL in brackets: [URL]
+  {
+    regex = '\\[(\\w+://\\S+?)\\]',
+    format = '$1',
+    highlight = 1,
+  },
+  -- Matches: a URL in curly braces: {URL}
+  {
+    regex = '\\{(\\w+://\\S+?)\\}',
+    format = '$1',
+    highlight = 1,
+  },
+  -- Matches: a URL in angle brackets: <URL>
+  {
+    regex = '<(\\w+://\\S+?)>',
+    format = '$1',
+    highlight = 1,
+  },
+  -- Then handle URLs not wrapped in brackets
+  -- regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
+  {
+    regex = '(?<![\\(\\{\\[<])\\b\\w+://\\S+',
+    format = '$0',
+  },
+  -- implicit mailto link
+  {
+    regex = '\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b',
+    format = 'mailto:$0',
+  },
+}
 
 config.tls_clients = {}
 
