0c1b8a84db84f33148027715264b5928753fd369
YI <uuuuuu@protonmail.com>
2026-04-10 15:26:00 +0800

    nixos: simplify tuned

--- nix/modules/power-management/tuned.nix
+++ nix/modules/power-management/tuned.nix
@@ -32,122 +32,121 @@ in
     in
     {
       services.tuned.enable = true;
+      services.tuned.ppdSupport = lib.mkDefault true;
       services.tuned.settings = {
         daemon = lib.mkDefault true;
       };
 
-      environment.etc = {
-        "tuned/profiles/${extreme-powersave}/script.sh" = {
-          source =
-            let
-              getShellArgs =
-                with lib;
-                let
-                  hasExtension = name: builtins.match ".*\\.[a-z]+" name != null;
-                  addExtension = name: if (hasExtension name) then name else "${name}.service";
-                  f = unit: escapeShellArg (addExtension unit);
-                in
-                list: concatMapStringsSep " " f list;
-              systemUnits = getShellArgs [
-                "pipewire*"
-                "vboxnet0"
-                "tailscaled"
-                "wstunnel"
-                "wg-quick-*"
-                "eternal-terminal"
-                "promtail"
-                "prometheus"
-                "prometheus-*.service"
-                "cadvisor"
-                "docker"
-                "docker.socket"
-                "docker-*.service"
-                "zerotierone"
-                "zerotierone-*"
-                "glusterd"
-                "glustereventsd"
-                "syncthing"
-                "aria2"
-                "chronyd"
-                "cups-browsed"
-                "nfs-idmapd"
-                "nfs-mountd"
-                "nfsdcld"
-                "pcscd*"
-                "rpc-statd"
-                "rpcbind*"
-                "system-samba.slice"
-                "vsftpd"
-                "waydroid-container"
-                "avahi-daemon*"
-                "postfix"
-                "ydotoold"
-                "caddy"
-                "system-cups.slice"
-              ];
-              userUnits = getShellArgs [
-                "pipewire*"
-                "syncthing"
-                "tomat"
-                "emacs"
-                "auto-fix-vscode-server"
-                "app-unison.slice"
-                "offlineimap*"
-              ];
-            in
-            pkgs.writeShellScript extreme-powersave ''
-              set -x
+      environment.etc."tuned/profiles/${extreme-powersave}/script.sh" = {
+        source =
+          let
+            getShellArgs =
+              with lib;
+              let
+                hasExtension = name: builtins.match ".*\\.[a-z]+" name != null;
+                addExtension = name: if (hasExtension name) then name else "${name}.service";
+                f = unit: escapeShellArg (addExtension unit);
+              in
+              list: concatMapStringsSep " " f list;
+            systemUnits = getShellArgs [
+              "pipewire*"
+              "vboxnet0"
+              "tailscaled"
+              "wstunnel"
+              "wg-quick-*"
+              "eternal-terminal"
+              "promtail"
+              "prometheus"
+              "prometheus-*.service"
+              "cadvisor"
+              "docker"
+              "docker.socket"
+              "docker-*.service"
+              "zerotierone"
+              "zerotierone-*"
+              "glusterd"
+              "glustereventsd"
+              "syncthing"
+              "aria2"
+              "chronyd"
+              "cups-browsed"
+              "nfs-idmapd"
+              "nfs-mountd"
+              "nfsdcld"
+              "pcscd*"
+              "rpc-statd"
+              "rpcbind*"
+              "system-samba.slice"
+              "vsftpd"
+              "waydroid-container"
+              "avahi-daemon*"
+              "postfix"
+              "ydotoold"
+              "caddy"
+              "system-cups.slice"
+            ];
+            userUnits = getShellArgs [
+              "pipewire*"
+              "syncthing"
+              "tomat"
+              "emacs"
+              "auto-fix-vscode-server"
+              "app-unison.slice"
+              "offlineimap*"
+            ];
+          in
+          pkgs.writeShellScript extreme-powersave ''
+            set -x
 
-              start() {
-                  systemctl stop ${systemUnits}
-                  systemctl --user --machine=e@ stop ${userUnits}
-                  return 0
-              }
+            start() {
+                systemctl stop ${systemUnits}
+                systemctl --user --machine=e@ stop ${userUnits}
+                return 0
+            }
 
-              stop() {
-                  systemctl list-unit-files --state=enabled --output json ${systemUnits} | \
-                      ${lib.getExe pkgs.jq} -r '.[].unit_file' | \
-                      xargs --no-run-if-empty --verbose systemctl start
+            stop() {
+                systemctl list-unit-files --state=enabled --output json ${systemUnits} | \
+                    ${lib.getExe pkgs.jq} -r '.[].unit_file' | \
+                    xargs --no-run-if-empty --verbose systemctl start
 
-                  systemctl list-unit-files --user --machine=e@ --state=enabled --output json ${userUnits} | \
-                      ${lib.getExe pkgs.jq} -r '.[].unit_file' | \
-                      xargs --no-run-if-empty --verbose systemctl start --user --machine=e@
+                systemctl list-unit-files --user --machine=e@ --state=enabled --output json ${userUnits} | \
+                    ${lib.getExe pkgs.jq} -r '.[].unit_file' | \
+                    xargs --no-run-if-empty --verbose systemctl start --user --machine=e@
 
-                  return 0
-              }
+                return 0
+            }
 
-              process() {
-                  ARG="$1"
-                  shift
-                  case "$ARG" in
-                      start)
-                          start "$@"
-                          RETVAL=$?
-                          ;;
-                      stop)
-                          stop "$@"
-                          RETVAL=$?
-                          ;;
-                      verify)
-                          if declare -f verify &> /dev/null; then
-                              verify "$@"
-                          else
-                              :
-                          fi
-                          RETVAL=$?
-                          ;;
-                      *)
-                          echo "Usage: $0 {start|stop|verify}"
-                          RETVAL=2
-                          ;;
-                  esac
-                  exit $RETVAL
-              }
+            process() {
+                ARG="$1"
+                shift
+                case "$ARG" in
+                    start)
+                        start "$@"
+                        RETVAL=$?
+                        ;;
+                    stop)
+                        stop "$@"
+                        RETVAL=$?
+                        ;;
+                    verify)
+                        if declare -f verify &> /dev/null; then
+                            verify "$@"
+                        else
+                            :
+                        fi
+                        RETVAL=$?
+                        ;;
+                    *)
+                        echo "Usage: $0 {start|stop|verify}"
+                        RETVAL=2
+                        ;;
+                esac
+                exit $RETVAL
+            }
 
-              process "$@"
-            '';
-          mode = "0555";
-        };
+            process "$@"
+          '';
+        mode = "0555";
       };
       services.tuned.profiles.${extreme-powersave} = {
         main = {
