ab703680d9636885a69485d93467a4729787b0de
YI <uuuuuu@protonmail.com>
2026-07-17 23:29:47 +0800

    home manager: add obs studio config

--- nix/hosts/home/e@aol.nix
+++ nix/hosts/home/e@aol.nix
@@ -1,6 +1,15 @@
 {
+  pkgs,
   ...
 }:
 {
   prefs.emacs.enable = true;
+  home.packages = with pkgs; [ mediamtx ];
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-pipewire-audio-capture
+    ];
+  };
 }
