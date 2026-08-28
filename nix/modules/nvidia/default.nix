695ab383bde2678a08cce2c0a3c8798b21fbd371
YI <uuuuuu@protonmail.com>
2026-08-28 17:25:23 +0800

    nixos: move nixos-cuda substituters to nvidia module

--- nix/modules/nvidia/default.nix
+++ nix/modules/nvidia/default.nix
@@ -26,5 +26,11 @@ in
       inherit (cfg) open;
     };
     nixpkgs.config.cudaSupport = cfg.enableNixpkgsCudaSupport;
+    nix = lib.mkIf nvidiaEnabled {
+      settings = {
+        substituters = [ "https://cache.nixos-cuda.org" ];
+        trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
+      };
+    };
   };
 }
