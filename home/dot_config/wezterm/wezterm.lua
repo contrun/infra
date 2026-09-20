8fab8f063e853370cc7c15c898537efc45d0c678
YI <uuuuuu@protonmail.com>
2026-09-20 16:44:49 +0800

    home/wezterm: tune appearance

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -58,34 +58,6 @@ local function spawn_editor(window, pane, path)
   )
 end
 
-wezterm.on('update-status', function(window)
-  -- Grab the utf8 character for the "powerline" left facing
-  -- solid arrow.
-  local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
-
-  -- Grab the current window's configuration, and from it the
-  -- palette (this is the combination of your chosen colour scheme
-  -- including any overrides).
-  local color_scheme = window:effective_config().resolved_palette
-  local bg = color_scheme.background
-  local fg = color_scheme.foreground
-
-  window:set_right_status(wezterm.format({
-    -- First, we draw the arrow...
-    { Background = { Color = 'none' } }, { Foreground = { Color = bg } },
-    { Text = SOLID_LEFT_ARROW }, -- Then we draw our text
-    { Background = { Color = bg } }, { Foreground = { Color = fg } },
-    { Text = ' ' .. wezterm.hostname() .. ' ' }
-  }))
-end)
-
--- Show which key table is active in the status area
-wezterm.on('update-right-status', function(window, pane)
-  local name = window:active_key_table()
-  if name then name = 'TABLE: ' .. name end
-  window:set_right_status(name or '')
-end)
-
 wezterm.on('format-tab-title', function(tab)
   local pane = tab.active_pane
   local title = pane.title
@@ -454,6 +426,7 @@ config.key_tables = {
   },
 }
 
+config.tab_bar_at_bottom = true
 config.scrollback_lines = 100000
 config.enable_scroll_bar = true
 
